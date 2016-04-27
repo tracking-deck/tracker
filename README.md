aad08e22e5e4e3beff9142ad20248ddfe3b62b4a
Christian Kohler <chko@zuehlke.com>
2016-04-27 15:19:34 +0200

    inital

---
+++ README.md
@@ -0,0 +1,8 @@
+# Getting Started
+```
+npm i
+```
+
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var paths = {
+    app : path.resolve(__dirname, './src'),
+    nodemodules: path.resolve(__dirname, 'node_modules')
+};
+
+module.exports = {
+    entry: path.resolve(paths.app, "app.js"),
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['', '.js'],
+        alias: {
+            'tracking': path.resolve(paths.nodemodules, 'tracking/build/tracking.js')
+        }
+    },
+    devServer: {
+        contentBase: paths.app,
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: [/node_modules/], loader: 'babel-loader', query: {
+          presets: ['es2015']
+        } },
+            { test: /\.html$/, loader: 'raw' }
+        ]
+    },
+    plugins: [
+            new webpack.ProvidePlugin({
+                tracking: "tracking",
+                'window.tracking': "tracking"
+            })
+        ],
+};
