049a485afd9a78d293e7633afae4d2b1fd5cb999
Peter Gfader <pgf@zuehlke.com>
2016-05-10 10:03:14 +0200

    color update. other webcam in use

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ export default {
     deltaMax: 3500,
     minDimension: 17,
     minGroupSize: 100,
-    refColor: "ca566c" //Previous colors: 9f172d ,  c90000,
+    refColor: "ff465a" //Previous colors: ca566c, 9f172d,  c90000,
 }
