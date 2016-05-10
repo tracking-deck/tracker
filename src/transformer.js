46ddcafb08004efa8d1b061f129f25264e0def0f
Peter Gfader <pgf@zuehlke.com>
2016-05-10 09:56:12 +0200

    update perspectives only 1st time

--- src/transformer.js
+++ src/transformer.js
@@ -4,14 +4,20 @@ import config from './config';
 class Transformer {
     constructor(srcCorners) {
         this.dstCorners = [0, 0, config.canvasWidth, 0, config.canvasWidth, config.canvasWidth / config.screenRatio, 0, config.canvasWidth / config.screenRatio];
-        this.updatePerspective(srcCorners);
     }
 
     updatePerspective(srcCorners) {
-        this.perspT = PerspT(srcCorners || this.dstCorners, this.dstCorners);
+        if (!this.perspT) {
+            console.log('PerspT', srcCorners, this.dstCorners);
+            this.perspT = PerspT(srcCorners, this.dstCorners);
+        }
     }
 
     transform({x, y}) {
+        if (!this.perspT) {
+            return {x: x, y: y};
+        }
+
         const result = this.perspT.transform(x, y);
         return {
             x: result[0],
