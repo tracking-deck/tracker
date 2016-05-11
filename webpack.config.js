f6bbe60cd641037537f5d303525284014f1025b2
Stefan Jäger <stefanjaeger@bluewin.ch>
2016-05-11 15:13:26 +0200

    first version of AI

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ var paths = {
     tracker : path.resolve(__dirname, './tracker'),
     beamer : path.resolve(__dirname, './beamer'),
     admin : path.resolve(__dirname, './admin'),
+    ai : path.resolve(__dirname, './ai'),
     nodemodules: path.resolve(__dirname, 'node_modules')
 };
 
@@ -14,7 +15,8 @@ module.exports = {
     entry: {
         tracker: path.resolve(paths.tracker, "app.js"),
         beamer: path.resolve(paths.beamer, "app.js"),
-        admin: path.resolve(paths.admin, "app.js")
+        admin: path.resolve(paths.admin, "app.js"),
+        ai: path.resolve(paths.ai, "app.js")
     },
     output: {
         path: paths.dist,
@@ -54,6 +56,11 @@ module.exports = {
             new HtmlWebpackPlugin({
               filename: 'admin.html',
               template: 'admin/index.html',
+              inject: false
+            }),
+            new HtmlWebpackPlugin({
+              filename: 'ai.html',
+              template: 'ai/index.html',
               inject: false
             })
         ],
